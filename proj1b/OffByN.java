5745e9f2eba637f86e4adf425076292d330dbfa6
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-03-25 01:02:17 +0100

    Add files via upload

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,9 +1,10 @@
 public class OffByN implements CharacterComparator {
     private int offset;
     public OffByN(int n) {
-        offset = n;
+        offset = Math.abs(n);
     }
 
+    @Override
     public boolean equalChars(char x, char y) {
         //TODO do I need null check?
         if (Character.isAlphabetic(x) && Character.isAlphabetic(y)) {
